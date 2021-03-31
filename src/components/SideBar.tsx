358ee3dc60ccd602fefe05bcb6d9d32b7df46875
viniciusoliveiras <vinitag190@gmail.com>
2021-03-31 15:36:28 -0300

    Content component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,10 +1,6 @@
-import { useEffect, useState } from 'react';
 import { Button } from '../components/Button';
 
-import { api } from '../services/api';
-
 import '../styles/global.scss';
-
 import '../styles/sidebar.scss';
 
 interface GenreResponseProps {
@@ -13,45 +9,17 @@ interface GenreResponseProps {
   title: string;
 }
 
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
+interface Props {
+  selectedGenreId: number;
+  setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
+  genres: GenreResponseProps[];
 }
 
-export function SideBar() {
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
-  const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
-    {} as GenreResponseProps
-  );
-
-  useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then((response) => {
-      setGenres(response.data);
-    });
-  }, []);
-
-  useEffect(() => {
-    api
-      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
-      .then((response) => {
-        setMovies(response.data);
-      });
-
-    api
-      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
-      .then((response) => {
-        setSelectedGenre(response.data);
-      });
-  }, [selectedGenreId]);
-
+export function SideBar({
+  selectedGenreId,
+  setSelectedGenreId,
+  genres,
+}: Props) {
   function handleClickButton(id: number) {
     setSelectedGenreId(id);
   }
@@ -65,6 +33,7 @@ export function SideBar() {
       <div className='buttons-container'>
         {genres.map((genre) => (
           <Button
+            key={String(genre.id)}
             title={genre.title}
             iconName={genre.name}
             onClick={() => handleClickButton(genre.id)}
